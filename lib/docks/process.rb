cbf6e6b22b5ecb4c31697c0f22f11d53beee2b7c
Chris Sauve <chris.sauve@shopify.com>
2014-11-13 14:31:08 -0500

    Updated Process class name

--- lib/docks/process.rb
+++ lib/docks/process.rb
@@ -1,43 +1,5 @@
 module Docks
   class Process
-    def initialize
-      @processors = {}
-    end
-
-    def register(processor, *params)
-      params.each do |param|
-        if Docks::SUPPORTED_PARAMS.include?(param.to_s)
-          @processors[param] ||= []
-          @processors[param] << processor
-        end
-      end
-    end
-
-    def process(parsed_file)
-      return nil if parsed_file.nil?
-      parsed_file.each_key do |key|
-        parsed_file[key] = process_param parsed_file[key], key
-      end
-
-      parsed_file
-    end
-
-    private
-
-    def process_param(content, param)
-      processed = content
-
-      unless @processors[param].nil?
-        @processors[param].each do |processor|
-          processed = processor.process processed
-        end
-      end
-
-      processed
-    end
-  end
-
-  class Process2
     def process_tag(tag, content)
       actions = Docks::Tags.processors_for(tag.to_sym)
       return content unless actions.kind_of?(Array)
