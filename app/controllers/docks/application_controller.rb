82804c5ff15db69d4b9070c5948bfe2d69dcfd17
Chris Sauve <chris.sauve@shopify.com>
2015-02-26 09:55:23 -0500

    maybe?

--- app/controllers/docks/application_controller.rb
+++ app/controllers/docks/application_controller.rb
@@ -1,25 +1,6 @@
 module Docks
   class ApplicationController < ::ApplicationController
-    layout "docks/application"
-    layout "docks/demo", only: [:demo]
-
+    layout 'docks/application'
     respond_to :html
-
-    def index
-    end
-
-    def pattern
-      @name = params[:pattern]
-
-      pattern = @name.gsub(/\-+/, "_")
-      @pattern = Docks::Cache.pattern_for(pattern)
-      @pattern_groups = Docks::Cache.pattern_groups
-
-      render(Docks.template_for(pattern) || :pattern)
-    end
-
-    def demo
-      @demo = Docks::Cache.pattern_for(params[:pattern].gsub(/\-+/, "_")).demo_for(params[:demo])
-    end
   end
 end
